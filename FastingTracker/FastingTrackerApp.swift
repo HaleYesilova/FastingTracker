00baff2c3959cc09702060aee8620b62362ba4b1
haledill <hale@dillapps.com>
2025-02-16 20:23:46 +0300

    Initial Commit

---
+++ FastingTracker/FastingTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTrackerApp.swift
+//  FastingTracker
+//
+//  Created by Halenur Yeşilova on 16.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FastingTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FastingTrackerTests/FastingTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTrackerTests.swift
+//  FastingTrackerTests
+//
+//  Created by Halenur Yeşilova on 16.02.2025.
+//
+
+import Testing
+@testable import FastingTracker
+
+struct FastingTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
